9472e5748522817f1bced25c8a1d7c545cf38fb0
Leandro Henrique <leandro2600@gmail.com>
2022-11-22 20:00:14 -0300

    Incluindo um loading simples

--- first-project-react/src/App.tsx
+++ first-project-react/src/App.tsx
@@ -3,6 +3,7 @@ import { Movie } from './types/Movie';
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadMovies();
@@ -19,23 +20,34 @@ const App = () => {
   }*/
 
   const loadMovies = async () => {
+    setLoading(true);
     let response = await fetch('https://api.b7web.com.br/cinema/');
     let json = await response.json();
+    setLoading(false);
     setMovies(json);
   }
 
   return (
     <div>
-      Total de filmes: {movies.length}
 
-      <div className='grid grid-cols-6 gap-3'>
-        {movies.map((item, index) => (
-          <div key={index}>
-            <img src={item.avatar} alt="Filme" className='w-32 block' />
-            {item.titulo}
+      {loading &&
+        <div>Carregando...</div>
+      }
+
+      {!loading &&
+        <>
+          Total de filmes: {movies.length}
+
+          <div className='grid grid-cols-6 gap-3'>
+            {movies.map((item, index) => (
+              <div key={index}>
+                <img src={item.avatar} alt="Filme" className='w-32 block' />
+                {item.titulo}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      }
     </div>
   );
 }
